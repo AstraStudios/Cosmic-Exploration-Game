3927beef4197546b4000cd8f906fde9061ecbeff
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-19 13:40:49 -0500

    Feature:UI Manager and game loop finished

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class Player : GenericSingleton<Player>
 {
-    float health = 100f; 
+    public float health = 100f; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100f;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(health);
-        if (health < 0)
-        {
-            Time.timeScale = 0;
-            GunController gunController = gameObject.GetComponentInChildren<GunController>();
-            gunController.canShoot = false;
-        }
+        Death();
     }
 
     public void TakeDamage()
     {
         health -= 10f;
+    }
+
+    void Death()
+    {
+        if (health <= 0)
+        {
+            Time.timeScale = 0;
+            GunController gunController = gameObject.GetComponentInChildren<GunController>();
+            gunController.canShoot = false;
+        }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text healthText;
+    [SerializeField] Player player;
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject losePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = player.health.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mainPanel.SetActive(false);
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (player.health <= 0)
+        {
+            mainPanel.SetActive(false);
+            losePanel.SetActive(true);
+        }
+    }
+
+    public void EndPauseMenu()
+    {
+        mainPanel.SetActive(true);
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void LoadBacktoMain()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
